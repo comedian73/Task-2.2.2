bc28fd40eb8081b621a9e8a12f762b3722cd0664
comedian73 <118024172+comedian73@users.noreply.github.com>
2023-07-06 15:55:34 +0300

    1

--- src/main/java/web/controller/CarController.java
+++ src/main/java/web/controller/CarController.java
@@ -20,11 +20,6 @@ public class CarController {
             int allCars, Model model) {
         CarService carService = new CarServiceImp();
         List<Car> list = new ArrayList<>();
-        list.add(new Car("Ford", "Mustang", 20000));
-        list.add(new Car("KIA", "Sportage", 13000));
-        list.add(new Car("Lamborghini", "350GT", 50000));
-        list.add(new Car("Renault", "LOGAN", 5000));
-        list.add(new Car("VAZ Lada", "2106", 3000));
         list = carService.carsCount(list, allCars);
         model.addAttribute("list", list);
 
